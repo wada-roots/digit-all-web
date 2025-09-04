3153484facda37ec7e56256ef84b351199c8fa17
codSoft237 <kiokokelvin237@gmail.com>
2025-09-04 19:17:10 +0300

    depl

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { createServer } from "./server";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base:"/Home/",
+    base:"/dms/",
     host: "::",
     port: 8080,
     fs: {
